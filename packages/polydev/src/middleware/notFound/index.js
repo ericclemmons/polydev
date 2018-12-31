75b932bb0a9cbf0feb87289a2a2cad3b5216894f
Eric Clemmons <eric@smarterspam.com>
2018-12-30 21:13:02 -0800

    404 page lets you create the missing page

--- packages/polydev/src/middleware/notFound/index.js
+++ packages/polydev/src/middleware/notFound/index.js
@@ -1,20 +1,58 @@
-export default function notFound(req, res) {
-  res.status(404).send(`
-    <body class="error">
-      <link href="https://fonts.googleapis.com/css?family=Quicksand:300,500" rel="stylesheet">
-      <link href="./styles.css" rel="stylesheet">
+import express from "express"
+import jetpack from "fs-jetpack"
+import opn from "opn"
+import path from "path"
 
-      <div id="splash"></div>
+export default express().use(
+  function getNotFound(req, res, next) {
+    if (req.method !== "GET") {
+      return next()
+    }
 
-      <main>
-        <h1>
-          <code>404</code> Not Found
-        </h1>
+    res.status(404).send(`
+      <head>
+        <link href="https://fonts.googleapis.com/css?family=Quicksand:300,500" rel="stylesheet">
+        <link href="./styles.css" rel="stylesheet">
+      </head>
+      <body class="error">
 
-        <p>
-          <kbd>${req.url}</kbd>
-        </p>
-      </main>
-    </body>
-  `)
-}
+        <div id="splash"></div>
+
+        <section>
+          <main>
+            <h1>
+              <code>404</code> Not Found
+            </h1>
+
+            <p>
+              <kbd>${req.path}</kbd>
+            </p>
+          </main>
+
+          <footer>
+            <form method="post">
+              <button>Create <code>./routes${req.path}/index.js</code></button>
+            </form>
+          </footer>
+        </section>
+      </body>
+    `)
+  },
+  function postCreateRoute(req, res, next) {
+    if (req.method !== "POST") {
+      return next()
+    }
+
+    const filepath = path.join(process.cwd(), "routes", req.path, "index.js")
+
+    // Ensure the file exists
+    jetpack.file(filepath)
+
+    // Open the file
+    opn(`vscode://file/${filepath}`)
+
+    // Reload the page by going to the path in the browser
+    // (we don't want the port!)
+    res.redirect(req.originalUrl)
+  }
+)
