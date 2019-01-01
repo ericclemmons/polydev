0ffdc17b8765c8d9c283f1b646d28ae0addc5e05
Eric Clemmons <eric@smarterspam.com>
2018-12-31 18:47:51 -0800

    Add hit counter to express example

--- packages/express-example/index.js
+++ packages/express-example/index.js
@@ -1,16 +1,30 @@
 const express = require("express")
 
+let hits = 0
+
 module.exports = express().get("/", (req, res) => {
+  hits++
+
   res.send(`
       <link href="https://fonts.googleapis.com/css?family=Quicksand:300,500" rel="stylesheet">
       <link href="./styles.css" rel="stylesheet">
 
       <div id="splash"></div>
 
-      <main>
-        <h1>
-          👋 Howdy from <kbd>express</kbd>
-        </h1>
-      </main>
+      <section>
+        <main>
+          <h1>
+            👋 Howdy from <kbd>express</kbd>
+          </h1>
+
+          <p>
+            <kbd>${hits}</kbd> ${hits ? "hits" : "hit"}
+          </p>
+        </main>
+
+        <footer>
+          <a href="/">&laquo; Back</a>
+        </footer>
+      </section>
     `)
 })
