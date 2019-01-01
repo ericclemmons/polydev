99e5d30c6e9175671812d0d28c94b4806aee431b
Eric Clemmons <eric@smarterspam.com>
2018-12-31 19:11:03 -0800

    Move server to separate file

---
+++ packages/polydev/src/server.js
@@ -0,0 +1,32 @@
+import express from "express"
+import opn from "opn"
+
+import { assets, error, notFound, router } from "./middleware"
+
+const { PORT = 3000 } = process.env
+
+process.on("uncaughtException", error => {
+  // TODO Youch
+  console.error("uncaughtException", error)
+})
+
+process.on("unhandledRejection", error => {
+  // TODO Youch
+  console.error("unhandledRejection", error)
+})
+
+const proxy = express()
+  .use(assets)
+  .use(router)
+  .use(notFound)
+  .use(error)
+
+const server = proxy.listen(PORT, () => {
+  const url = `http://localhost:${server.address().port}/`
+
+  console.log(`🚀 Ready! ${url}`)
+
+  if (process.argv.includes("--open")) {
+    opn(url)
+  }
+})
