f57bb7df748ccf24b22bdd45c67dace8df5f5f95
Eric Clemmons <eric@smarterspam.com>
2019-01-01 22:11:38 -0800

    Prettier Next.js example

--- packages/next-example/pages/index.js
+++ packages/next-example/pages/index.js
@@ -1,1 +1,28 @@
-export default (req, res) => <h1>Howdy there!</h1>
+import Head from "next/head"
+import { Fragment } from "react"
+
+export default (req, res) => (
+  <Fragment>
+    <Head>
+      <link
+        href="https://fonts.googleapis.com/css?family=Quicksand:300,500"
+        rel="stylesheet"
+      />
+      <link href="/styles.css" rel="stylesheet" />
+    </Head>
+
+    <div id="splash" />
+
+    <section>
+      <main>
+        <h1>
+          👋 Howdy from <kbd>Next.js</kbd>
+        </h1>
+      </main>
+
+      <footer>
+        <a href="/">&laquo; Back</a>
+      </footer>
+    </section>
+  </Fragment>
+)
