659c58d5cd0b0e4490e76fd49d5701963cc07a9f
Eric Clemmons <eric@smarterspam.com>
2019-01-07 22:08:46 -0800

    GraphQL doesn't need to use express explicitly

--- packages/graphql-example/index.js
+++ packages/graphql-example/index.js
@@ -1,12 +1,8 @@
-const express = require("express")
 const graphql = require("express-graphql")
-
 const schema = require("./schema")
 
-module.exports = express().use(
-  graphql({
-    graphiql: true,
-    pretty: true,
-    schema
-  })
-)
+module.exports = graphql({
+  graphiql: true,
+  pretty: true,
+  schema
+})
