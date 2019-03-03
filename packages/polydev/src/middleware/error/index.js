59eaf0eb85452885afaa1381c859368b27ca8d68
Eric Clemmons <eric@smarterspam.com>
2019-03-02 20:24:19 -0800

    Prettier error page

--- packages/polydev/src/middleware/error/index.js
+++ packages/polydev/src/middleware/error/index.js
@@ -6,6 +6,27 @@ module.exports = function errorHandler(error, req, res, next) {
 
   const youch = new Youch(error, req)
 
+  youch.addLink((error) => {
+    return `
+      <link href="/_polydev/styles.css" rel="stylesheet">
+      <div id="splash"></div>
+    `
+  })
+
+  youch.addLink(({ message }) => {
+    const url = `https://google.com/search?q=${encodeURIComponent(message)}`
+
+    return `<a href="${url}" target="_blank" title="Search Google"><i class="fab fa-google"></i></a>`
+  })
+
+  youch.addLink(({ message }) => {
+    const url = `https://stackoverflow.com/search?q=${encodeURIComponent(
+      message
+    )}`
+
+    return `<a href="${url}" target="_blank" title="Search Stack Overflow"><i class="fab fa-stack-overflow"></i></a>`
+  })
+
   youch.toHTML().then((html) => {
     res.status(statusCode).send(html)
   })
